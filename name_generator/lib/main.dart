a7e425fb4bb9b29fe115902956391b40b44931bf
AJAkil <cyclosis47@gmail.com>
2021-02-03 00:30:37 +0600

    Added an english package

--- name_generator/lib/main.dart
+++ name_generator/lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // an english word object
+    final wordPair = WordPair.random();
+
     return MaterialApp(
       title: 'Welcome to flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('First Flutter App'),
+        ),
+        body: Center(
+          child: Text(wordPair.asCamelCase),
+        ),
+      ),
     );
   }
-
 }
+
+
