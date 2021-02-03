262d0caa770c6e9df6e3b3dfc2ab36d3921b4bd1
AJAkil <cyclosis47@gmail.com>
2021-02-04 01:02:44 +0600

    Changed the theme of the app.

--- name_generator/lib/main.dart
+++ name_generator/lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to flutter',
+      theme: ThemeData(
+        primaryColor: Colors.black,
+      ),
       home: RandomWords(),
     );
   }
